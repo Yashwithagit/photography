3006644439b8fb7c69d9ff8f655df07eb019b779
yashwitha-at-411977029542 <D/b20m+IOckszuzuPuS4+r+Yorqu2R1a8ymbIA6yjhI=>
2023-07-30 17:05:10 +0530

    FEAT:Link updated

--- lib/constant.ts
+++ lib/constant.ts
@@ -59,54 +59,44 @@ export const slideList = [
     id: 1,
     image: ImagePath.BackGroundImage,
     title: "The Way of Water",
-    subTitle:
-      "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-    interval: 0,
+    link: "https://www.digitalcameraworld.com/",
+
     event: false,
   },
   {
     id: 2,
     image: ImagePath.BackGroundImage,
     title: "Black Adam",
-    subTitle:
-      "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
-    interval: 500,
+    link: "https://www.digitalcameraworld.com/",
+
     event: false,
   },
   {
     id: 3,
     image: ImagePath.BackGroundImage,
     title: " Wakanda Forever",
-    subTitle:
-      "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
-    interval: 2500,
+    link: "https://www.digitalcameraworld.com/",
     event: false,
   },
   {
     id: 4,
     image: ImagePath.BackGroundImage,
     title: " Wakanda Forever",
-    subTitle:
-      "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
-    interval: 2500,
+    link: "https://www.digitalcameraworld.com/",
     event: false,
   },
   {
     id: 5,
     image: ImagePath.BackGroundImage,
     title: " Wakanda Forever",
-    subTitle:
-      "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
-    interval: 2500,
+    link: "https://www.digitalcameraworld.com/",
     event: false,
   },
   {
     id: 6,
     image: ImagePath.BackGroundImage,
     title: " Wakanda Forever",
-    subTitle:
-      "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
-    interval: 2500,
+    link: "https://www.digitalcameraworld.com/",
     event: false,
   },
 ];
